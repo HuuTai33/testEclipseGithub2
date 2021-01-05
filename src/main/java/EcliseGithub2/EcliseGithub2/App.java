9a1c94b2e03a482342f3daf3cde3076d1dec90ce
HuuTai33 <huutai.tran33@gmail.com>
2021-01-05 14:37:32 +0100

    Commit 1

---
+++ src/main/java/EcliseGithub2/EcliseGithub2/App.java
@@ -0,0 +1,13 @@
+package EcliseGithub2.EcliseGithub2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour EPSI !" );
+    }
+}
